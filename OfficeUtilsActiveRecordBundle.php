4066aa0cdfaaafff1098554059069e676f7b09fe
Roman Chernushkin <rchernushkin@omirussia.ru>
2016-02-09 23:56:36 +0300

    Init commit

---
+++ OfficeUtilsActiveRecordBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OfficeUtils\ActiveRecordBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OfficeUtilsActiveRecordBundle extends Bundle
+{
+}
---
+++ Service/ActiveRecordService.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cra
+ * Date: 09.02.2016
+ * Time: 13:59
+ */
+
+namespace OfficeUtils\ActiveRecordBundle\Service;
+
+
+class ActiveRecordService
+{
+
+    /**
+     * ActiveRecordService constructor.
+     */
+    public function __construct($newcfg)
+    {
+        \ActiveRecord\Config::initialize(function($cfg) use ($newcfg)
+        {
+            $cfg->set_model_directory($newcfg['model_directory']);
+            $cfg->set_connections($newcfg['connections']);
+            $cfg->set_default_connection($newcfg['default_connection']);
+
+            if($newcfg['sql_logging'] == 1)
+            {
+                $logger = \Log::singleton('file', $newcfg['sql_log_file'],'ident',array('mode' => 0664, 'timeFormat' =>  '%Y-%m-%d %H:%M:%S'));
+                $cfg->set_logging(true);
+                $cfg->set_logger($logger);
+            }
+        }
+        );
+
+    }
+}
